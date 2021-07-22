9760332d6979b3a1d83d846af0a88f339ddb7bde
= <anyou@mpad365.com>
2021-07-22 14:04:07 +0800

    frame

--- multifunc_sys/user_related/views.py
+++ multifunc_sys/user_related/views.py
@@ -2,4 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# 创建超级用户
+def create_superuser():
+    pass
+
+# 创建用户
+def create_user():
+    pass
+
+# 登录,账号密码登录
+def login_pw():
+    pass
+
+# 登录，电话号登录
+def login_tel():
+    pass
+
+# 登录，邮箱登录
+def login_email():
+    pass
+
+# 登出
+def logout():
+    pass
 
